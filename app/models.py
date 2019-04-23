d6a8ff063f30d31b2b269cd25e9a2d91543deaea
simon <gatheru.simon432@gmail.com>
2019-04-23 21:44:29 +0300

    establish relationship between users and pitches table

--- app/models.py
+++ app/models.py
@@ -28,7 +28,20 @@ class Pitch:
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255))
     username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitches(db.Model):
+    __tablename__='pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    pitch = db.Column(db.String(255))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
+    user = db.relationship('Users',backref='pitches', lazy="dynamic")
--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
-from app import app
+from app import app,db
+from app.models import Users,Pitches
+from flask_script import Manager, Server
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell():
+    return dict(app=app, db=db, Users=Users, Pitches=Pitches)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
