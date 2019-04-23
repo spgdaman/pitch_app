80fb07ad006ba07b3c02181e6dead761af7e8b81
simon <gatheru.simon432@gmail.com>
2019-04-23 10:58:22 +0300

    Navbar added to base template

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-# from app import views
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('navbar.html')
 
 
 if __name__ == '__main__':
